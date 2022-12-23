9b505bab74a469a51f8af963d18a1a452352e665
oberien <jaro.fietz@gmx.de>
2022-12-23 17:03:51 +0100

    Day16 part2 impossible

--- src/main/kotlin/day16/Day16.kt
+++ src/main/kotlin/day16/Day16.kt
@@ -10,23 +10,35 @@ class Day16 : Day {
     }
 
     override fun part2(input: String): String {
-        return ""
+        // runtime with this approach is insane
+        //  9 takes   4.5s
+        // 10 takes  21s
+        // 11 takes 142s
+        // -> exponential growth
+        // estimated time for 26 = 5^17 = 25.000 years
+        val graph = parse(input)
+        return dfsPart2(Memoization(), graph, graph.nodes["AA"]!!, graph.nodes["AA"]!!, 8, setOf()).toString()
     }
 }
 
 private class Memoization() {
     val memoization = HashMap<String, Int>()
 
-    fun insert(current: Valve, timeLeft: Int, turnedValves: Set<Valve>, max: Int) {
-        memoization[hash(current, timeLeft, turnedValves)] = max
+    fun insert(player1: Valve, player2: Valve?, timeLeft: Int, turnedValves: Set<Valve>, max: Int) {
+        memoization[hash(player1, player2, timeLeft, turnedValves)] = max
     }
 
-    fun get(current: Valve, timeLeft: Int, turnedValves: Set<Valve>): Int? =
-        memoization[hash(current, timeLeft, turnedValves)]
+    fun get(player1: Valve, player2: Valve?, timeLeft: Int, turnedValves: Set<Valve>): Int? =
+        memoization[hash(player1, player2, timeLeft, turnedValves)]
 
-    fun hash(current: Valve, timeLeft: Int, turnedValves: Set<Valve>): String {
+    fun hash(player1: Valve, player2: Valve?, timeLeft: Int, turnedValves: Set<Valve>): String {
+        val playerHash = if (player2 != null) {
+            listOf(player1.name, player2.name).sorted().joinToString("")
+        } else {
+            player1.name
+        }
         val turnedHash = turnedValves.asSequence().map { it.name }.sorted().joinToString("")
-        return "${current.name}:$timeLeft:$turnedHash"
+        return "${playerHash}:$timeLeft:$turnedHash"
     }
 }
 
@@ -34,7 +46,7 @@ private fun dfsPart1(memoization: Memoization, graph: Graph, current: Valve, tim
     if (timeLeft <= 1) {
         return 0
     }
-    val memoized = memoization.get(current, timeLeft, turnedValves)
+    val memoized = memoization.get(current, null, timeLeft, turnedValves)
     if (memoized != null) {
         return memoized
     }
@@ -52,7 +64,67 @@ private fun dfsPart1(memoization: Memoization, graph: Graph, current: Valve, tim
         val maxRest = dfsPart1(memoization, graph, graph.nodes[path]!!, timeLeft - 1, turnedValves)
         max = max(max, maxRest)
     }
-    memoization.insert(current, timeLeft, turnedValves, max)
+    memoization.insert(current, null, timeLeft, turnedValves, max)
+    return max
+}
+
+private fun dfsPart2(memoization: Memoization, graph: Graph, player1: Valve, player2: Valve, timeLeft: Int, turnedValves: Set<Valve>): Int {
+    if (timeLeft <= 1) {
+        return 0
+    }
+    val memoized = memoization.get(player1, player2, timeLeft, turnedValves)
+    if (memoized != null) {
+        return memoized
+    }
+
+    val valvePossibilities = mutableSetOf(Pair(false, false), Pair(true, false), Pair(false, true), Pair(true, true))
+    if (turnedValves.contains(player1)) {
+        valvePossibilities.remove(Pair(true, false))
+        valvePossibilities.remove(Pair(true, true))
+    }
+    if (turnedValves.contains(player2)) {
+        valvePossibilities.remove(Pair(false, true))
+        valvePossibilities.remove(Pair(true, true))
+    }
+    if (player1 == player2) {
+        valvePossibilities.remove(Pair(true, true))
+    }
+
+    var max = 0
+
+    val released1 = (timeLeft - 1) * player1.rate
+    val released2 = (timeLeft - 1) * player2.rate
+    for (possibility in valvePossibilities) {
+        when (possibility) {
+            Pair(true, true) -> {
+                val turned = HashSet(turnedValves).apply { add(player1); add(player2) }
+                val released = released1 + released2
+                val maxRest = dfsPart2(memoization, graph, player1, player2, timeLeft - 1, turned)
+                max = max(max, released + maxRest)
+            }
+            Pair(true, false) -> {
+                val turned = HashSet(turnedValves).apply { add(player1) }
+                for (path in player2.paths) {
+                    max = max(max, released1 + dfsPart2(memoization, graph, player1, graph.nodes[path]!!, timeLeft - 1, turned))
+                }
+            }
+            Pair(false, true) -> {
+                val turned = HashSet(turnedValves).apply { add(player2) }
+                for (path in player1.paths) {
+                    max = max(max, released2 + dfsPart2(memoization, graph, graph.nodes[path]!!, player2, timeLeft - 1, turned))
+                }
+            }
+            Pair(false, false) -> {
+                for (path1 in player1.paths) {
+                    for (path2 in player2.paths) {
+                        max = max(max, dfsPart2(memoization, graph, graph.nodes[path1]!!, graph.nodes[path2]!!, timeLeft - 1, turnedValves))
+                    }
+                }
+            }
+        }
+    }
+
+    memoization.insert(player1, player2, timeLeft, turnedValves, max)
     return max
 }
 
